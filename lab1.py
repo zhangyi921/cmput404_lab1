f74ea4c694a89aa3c0f152c1ff80d5cb10e6532e
Yi Zhang <yi16@cs.ualberta.ca>
2019-01-09 18:03:22 -0700

    print code

--- lab1.py
+++ lab1.py
@@ -1,6 +1,9 @@
 import requests
 print(requests.__version__)
-r = requests.get("http://google.com")
-print(r.status_code)
+#r = requests.get("http://google.com")
+#print(r.status_code)
+#print(r.text)
+#print(dir(r))
+
+r = requests.get("https://raw.githubusercontent.com/zhangyi921/cmput404_lab1/master/lab1.py")
 print(r.text)
-print(dir(r))
